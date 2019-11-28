ed30cbc6a392aa1bdeeb0a1d6ef1cc6133cb91d1
Duckmaster <hello14145@gmail.com>
2019-11-28 21:16:53 +0000

    Added dataclass

--- duck-api/api.py
+++ duck-api/api.py
@@ -1,6 +1,8 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.requests import HTTPConnection
+from dataclasses import dataclass
+import motorcontrol
 
 app = Starlette()
 
@@ -13,3 +15,15 @@ async def forward(request: HTTPConnection):
 async def backward(request: HTTPConnection):
     print("go backward (eventually)")
     return JSONResponse({"moving": "backward"})
+
+@dataclass
+class movement:
+    waitTime: int
+
+    def init(self, func):
+        func()
+        return True
+    
+    def finish(self):
+        motorcontrol.stop()
+        return True
